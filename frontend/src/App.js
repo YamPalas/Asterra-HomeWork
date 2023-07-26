3ad3f8a86c2a550ee3f33dd72f2a7a4792309f1e
Yam Pallace <yampalas100@gmail.com>
2023-07-26 23:32:20 +0300

    The net ninja #4

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Hello ninjas</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
